94436bd49e5f3df51881a30de059d578e5d16eba
oki6551 <oki6551@naver.com>
2018-04-12 14:37:32 +0900

    Add files via upload

--- src/main/java/com/oki6551/calculator/Calculator.java
+++ src/main/java/com/oki6551/calculator/Calculator.java
@@ -1,6 +1,8 @@
 package com.oki6551.calculator;
 import org.springframework.stereotype.Service;
-@Service
+/**
+*@Service
+*/
 public class Calculator{
 	int sum(int a,int b){
 		return a+b;
--- src/main/java/com/oki6551/calculator/CalculatorApplication.java
+++ src/main/java/com/oki6551/calculator/CalculatorApplication.java
@@ -3,7 +3,9 @@ package com.oki6551.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+*@SpringBootApplication
+*/
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
